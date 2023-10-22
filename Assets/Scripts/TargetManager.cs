2dc9c8026a6a2cd1cdfbdb5bbff861689777f989
John-Ristum <125324914+John-Ristum@users.noreply.github.com>
2023-10-22 23:13:18 +1100

    Brief-3-Requirements
    Created 3 new target types.
    
    Random target will be instantiated at a random spawn point when the "I" key is pressed.

---
+++ Assets/Scripts/TargetManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetManager : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] targetTypes;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+            SpawnAtRandom();
+    }
+
+    /// <summary>
+    /// Spawns a random target at a random spawn point
+    /// </summary>
+    void SpawnAtRandom()
+    {
+        int rndTarget = Random.Range(0, targetTypes.Length);
+        int rndSpawn = Random.Range(0, spawnPoints.Length);
+        GameObject target = Instantiate(targetTypes[rndTarget], spawnPoints[rndSpawn].position, spawnPoints[rndSpawn].rotation);
+    }
+}
